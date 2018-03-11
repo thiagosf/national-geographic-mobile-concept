3c48eca048524ac40ce8265205ea6e4d0e86b6be
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2018-03-11 12:00:01 -0300

    WIP thumb slider and transition big image

--- public/js/app.js
+++ public/js/app.js
@@ -6,14 +6,37 @@ function initTinySlider() {
     container: '.home-slider-thumbs ul',
     items: 3,
     nav: false,
+    controls: true,
     slideBy: 1,
-    autoplay: false
+    autoplay: false,
+    mouseDrag: true
   });
   if (slider) {
-    slider.events.on('transitionEnd', function () {
-      console.log(arguments);
+    slider.events.on('transitionEnd', function (info) {
+      var id = info.container.children[info.index].getAttribute('data-id');
+      changeSlide(id);
     });
   }
+}
+
+function changeSlide(id) {
+  var items = [].slice.call(document.querySelectorAll('.home-slider-item'));
+  items.map(function (item) {
+    if (item.classList.contains('active')) {
+      item.classList.add('leaving');
+      setTimeout(function () {
+        item.classList.remove('active');
+        item.classList.remove('leaving');
+      }, 500);
+    }
+    if (item.getAttribute('data-id') === id) {
+      item.classList.add('entering');
+      setTimeout(function () {
+        item.classList.remove('entering');
+        item.classList.add('active');
+      }, 500);
+    }
+  });
 }
 
 window.addEventListener('load', initTinySlider);
